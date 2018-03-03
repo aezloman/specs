058307662efccd08d2999532d5c465c72918f30c
Aezlo Man <aezloman@athelo.net>
2018-03-03 16:49:41 +0000

    Create gtag.js

---
+++ .cn/gtag.js
@@ -0,0 +1,12 @@
+// Global site tag (gtag.js) - Google Analytics 
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+  gtag('config', 'UA-115047728-1');
+</script>
+
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-115047728-1';
+document.head.appendChild(imported);
