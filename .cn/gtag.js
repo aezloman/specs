b3d780283f65707ac5bde6af2a364696e4077727
Aezlo Man <aezloman@athelo.net>
2018-03-04 13:32:28 +0000

    Update gtag.js

--- .cn/gtag.js
+++ .cn/gtag.js
@@ -5,7 +5,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
   gtag('config', 'UA-115047728-1');
-</script>
 
 var imported = document.createElement('script');
 imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-115047728-1';
